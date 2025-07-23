86d8375a4074cd5850b8578cb19d1963688db7c7
0xaf2fasga <gaweg3qagfa@proton.me>
2025-07-23 15:28:39 +0800

    feat: add soon badge to button v2

--- src/components/button/v2.tsx
+++ src/components/button/v2.tsx
@@ -9,6 +9,7 @@ const ButtonV2 = (props: any) => {
     type = "primary",
     htmlType = "button",
     loading,
+    soon,
     ...restProps
   } = props;
   const size = useMemo(() => {
@@ -24,7 +25,7 @@ const ButtonV2 = (props: any) => {
     <button
       type={htmlType}
       className={clsx(
-        "shrink-0 font-[SpaceGrotesk] cursor-pointer flex justify-center items-center px-[17px] h-[32px] rounded-[8px] disabled:opacity-50 disabled:cursor-not-allowed",
+        "relative shrink-0 font-[SpaceGrotesk] cursor-pointer flex justify-center items-center px-[17px] h-[32px] rounded-[8px] disabled:opacity-50 disabled:cursor-not-allowed",
         type === "default" &&
           "border border-[#6A5D3A] text-white text-[14px] px-[10px] bg-black/20 [background-image:none]",
         type === "primary" &&
@@ -33,6 +34,13 @@ const ButtonV2 = (props: any) => {
       )}
       {...restProps}
     >
+      {
+        soon && (
+          <div className="absolute right-[-10px] top-[-10px] flex justify-center items-center text-[12px] font-[500] text-white h-[20px] px-[5px] rounded-[6px] bg-[#4C4C4C]">
+            soon
+          </div>
+        )
+      }
       {loading ? <Loading size={Math.min(size, 20)} /> : children}
     </button>
   );
