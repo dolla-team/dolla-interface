25bcb1fbc322f08244c132c1b35f7afab4cb838b
Luna <luna.spark9234@proton.me>
2025-07-24 10:14:10 +0800

    fix toast interface and remove some useless fonts.

--- src/sections/winners/use-last-winner.ts
+++ src/sections/winners/use-last-winner.ts
@@ -22,7 +22,7 @@ export default function useLastWinner() {
     } finally {
       window.winnerTimer = setTimeout(() => {
         fetLastWinner();
-      }, 5000);
+      }, 10000);
     }
   };
 
