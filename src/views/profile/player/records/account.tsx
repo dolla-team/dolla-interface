621d5ea16f6909a4010678b40dfa96cec57edf4c
0xaf2fasga <gaweg3qagfa@proton.me>
2025-07-29 12:00:50 +0800

    fix: add pool id and prize draw id in user records

--- src/views/profile/player/records/account.tsx
+++ src/views/profile/player/records/account.tsx
@@ -33,7 +33,11 @@ const Account = (props: any) => {
           return (
             <div className="flex items-center gap-[4px]">
               <div className="">{record.typeName}</div>
-              <div className="">#{record.pool_id}</div>
+              {
+                Big(record.pool_id || 0).gt(0) && (
+                  <div className="">#{record.pool_id}</div>
+                )
+              }
             </div>
           );
         }
@@ -41,12 +45,16 @@ const Account = (props: any) => {
           return (
             <div className="flex items-center gap-[4px]">
               <div className="">{record.typeName}</div>
-              <div className="">#{record.pool_id}</div>
+              {
+                Big(record.prize_draw_id || 0).gt(0) && (
+                  <div className="">#{record.prize_draw_id}</div>
+                )
+              }
             </div>
           );
         }
         return record.typeName;
-      } 
+      }
     },
     {
       dataIndex: "assets",
