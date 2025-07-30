c0b91d00b4edffd90056a03110327b76029e98c7
0xaf2fasga <gaweg3qagfa@proton.me>
2025-07-30 18:42:10 +0800

    fix: Fix incomplete display of portfolio buyer claim modal on mobile

--- src/views/profile/ components/claim/modal.tsx
+++ src/views/profile/ components/claim/modal.tsx
@@ -10,7 +10,7 @@ const ClaimModal = (props: any) => {
       open={open}
       onClose={onClose}
     >
-      <div className={clsx("w-[672px] h-[444px] flex flex-col items-stretch rounded-[16px] border border-[#6A5D3A] bg-[#35302B]", className)}>
+      <div className={clsx("w-[672px] h-[444px] max-md:w-full flex flex-col items-stretch rounded-[16px] border border-[#6A5D3A] bg-[#35302B] max-md:rounded-b-[0]", className)}>
         <div className="w-full shrink-0 flex justify-between items-center bg-black/20 rounded-t-[10px] p-[18px_23px_16px_31px] text-white text-[20px] font-[SpaceGrotesk] font-[700] leading-[100%]">
           <div className="">Claim</div>
           <button
@@ -21,7 +21,7 @@ const ClaimModal = (props: any) => {
             <img src="/profile/icon-close.svg" className="w-[10px] h-[12px] shrink-0" />
           </button>
         </div>
-        <div className="px-[20px] py-[18px] flex-1 h-0">
+        <div className="px-[20px] py-[18px] flex-1 h-0 max-md:px-[0px] max-md:pb-[0px]">
           <Index {...restProps} />
         </div>
       </div>
