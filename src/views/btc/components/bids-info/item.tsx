4ae395dda718cbc8c23297cce432d3e2c0afb7f3
Luna <luna.spark9234@proton.me>
2025-08-06 12:28:40 +0800

    fix bid info.

--- src/views/btc/components/bids-info/item.tsx
+++ src/views/btc/components/bids-info/item.tsx
@@ -4,17 +4,17 @@ import dayjs from "dayjs";
 
 export default function Item({ data }: any) {
   return (
-    <div className="w-[248px] h-[46px] mt-[10px] p-[8px] flex items-center rounded-[23px] border border-[#FFE9B2] bg-white/10 backdrop-blur-[10px] flex items-center justify-center">
+    <div className="w-[248px] h-[46px] mt-[10px] p-[6px] rounded-[23px] border border-[#FFE9B2] bg-white/10 backdrop-blur-[10px] flex items-center">
       <Avatar
         address={data.user}
         email={data.user_email}
         size={30}
-        className="border border-[#131417] mr-[8px]"
+        className="border border-[#131417] mr-[6px]"
       />
-      <div className="text-[14px] font-bold truncate mr-[5px]">
+      <div className="text-[14px] font-bold truncate mr-[3px] shrink-0">
         {formatAddress(data.user, 3)}
       </div>
-      <div className="text-[14px] font-bold text-[#FFEF43] mr-[5px]">
+      <div className="text-[14px] font-bold text-[#FFEF43] mr-[3px]">
         bid {data.times}
       </div>
       <div className="text-[14px] font-bold">{dayjs(data.time).fromNow()}</div>
