f17ca6e771a456e672e6f6a9b668146429a97c7a
Luna <luna.spark9234@proton.me>
2025-08-18 19:25:20 +0800

    est gas.

--- src/sections/est-gas/index.tsx
+++ src/sections/est-gas/index.tsx
@@ -7,11 +7,11 @@ export default function EstGas({ className }: any) {
 
   useEffect(() => {
     const fetchGas = async () => {
-      const gas = await axiosInstance.get('/api/v1/gas');
+      const gas = await axiosInstance.get("/api/v1/gas");
       setGas(gas.data.data);
-    }
+    };
     fetchGas();
-  }, [])
+  }, []);
 
   return (
     <div className="relative group">
@@ -19,22 +19,30 @@ export default function EstGas({ className }: any) {
         <img src="/est-gas.gif" className="w-[24px] h-[24px]" />
         <span className="text-[14px] text-[#FFE9B2]">Est. Gas</span>
       </div>
-      <div className="w-[288px] rounded-[10px] bg-[#35302B] border border-[#6A5D3A] leading-[2] absolute left-[-18px] top-[40px] text-[#FFE9B2] text-[14px] invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 py-[10px] px-[16px]">
+      <div className="w-[288px] rounded-[10px] bg-[#2D2B35] border border-[#514A5D] leading-[2] absolute left-[-18px] top-[40px] text-white text-[12px] invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 py-[10px] px-[16px]">
         <div className="flex items-center justify-between">
           <span>Bid</span>
-          <span>~ ${gas?.bid.min} - ${gas?.bid.max}</span>
+          <span>
+            ~ ${gas?.bid.min} - ${gas?.bid.max}
+          </span>
         </div>
         <div className="flex items-center justify-between">
           <span>Market Creation</span>
-          <span>~ ${gas?.market_creation.min} - ${gas?.market_creation.max}</span>
+          <span>
+            ~ ${gas?.market_creation.min} - ${gas?.market_creation.max}
+          </span>
         </div>
         <div className="flex items-center justify-between">
           <span>Withdraw</span>
-          <span>~ ${gas?.withdraw.min} - ${gas?.withdraw.max}</span>
+          <span>
+            ~ ${gas?.withdraw.min} - ${gas?.withdraw.max}
+          </span>
         </div>
         <div className="flex items-center justify-between">
           <span>Claim</span>
-          <span>~ ${gas?.claim.min} - ${gas?.claim.max}</span>
+          <span>
+            ~ ${gas?.claim.min} - ${gas?.claim.max}
+          </span>
         </div>
       </div>
     </div>
