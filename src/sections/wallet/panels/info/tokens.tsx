c772f4d976c2e3d71cfed5ab4820c274eac83034
Luna <luna.spark9234@proton.me>
2025-09-29 19:10:39 +0800

    wallet tokens.

--- src/sections/wallet/panels/info/tokens.tsx
+++ src/sections/wallet/panels/info/tokens.tsx
@@ -1,50 +1,86 @@
-import { QUOTE_TOKEN } from "@/config/btc";
-import Empty from "./empty";
 import { useAuth } from "@/contexts/auth";
 import { formatNumber } from "@/utils/format/number";
 import Big from "big.js";
+import { BASE_TOKEN, QUOTE_TOKEN } from "@/config/btc";
+import useTokenPrice from "@/hooks/use-token-price";
+import { useMemo } from "react";
 
-export default function Tokens({ onDeposit }: { onDeposit: () => void }) {
-  const { quoteTokenBalance } = useAuth() || {};
+export default function Tokens() {
+  const { nearAccount } = useAuth() || {};
 
-  if (Big(quoteTokenBalance || 0).eq(0)) {
-    return <Empty onDeposit={onDeposit} />;
-  }
+  const tokenIds = useMemo(() => {
+    return [
+      {
+        chain: "near",
+        address: QUOTE_TOKEN?.address
+      },
+      {
+        chain: "near",
+        address: BASE_TOKEN?.address
+      }
+    ];
+  }, [QUOTE_TOKEN, BASE_TOKEN]);
+
+  const { prices } = useTokenPrice(tokenIds);
 
   return (
     <div>
-      <Item balance={quoteTokenBalance} price={1} />
+      <Item
+        balance={nearAccount?.balance}
+        price={prices[0]?.last_price || 1}
+        token={QUOTE_TOKEN}
+      />
+      <Item
+        balance={nearAccount?.prizeBalance}
+        price={prices[1]?.last_price || 1}
+        token={BASE_TOKEN}
+      />
     </div>
   );
 }
 
-const Item = ({ balance, price }: { balance: string; price: number }) => {
+const Item = ({
+  balance,
+  price,
+  token
+}: {
+  balance: string;
+  price: number;
+  token: any;
+}) => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center mb-[10px]">
       <div className="flex items-center gap-[14px]">
         <div className="w-[32px] h-[32px] rounded-full relative">
-          <img src={QUOTE_TOKEN.icon} className="w-full h-full object-cover" />
-          <img
+          <img src={token.icon} className="w-full h-full object-cover" />
+          {/* <img
             src="/chains/bera-1.png"
             className="w-[16px] h-[16px] absolute bottom-[-4px] right-[-4px]"
-          />
+          /> */}
         </div>
         <div>
           <div>
-            <span className="text-[14px] text-black">
-              {QUOTE_TOKEN.symbol}{" "}
-            </span>
+            <span className="text-[14px] text-black">{token.symbol} </span>
             {/* <span className="text-[12px] text-[#8A87AA]">(Near)</span> */}
           </div>
-          <div className="text-[12px] text-[#8A87AA]">$1.00</div>
+          <div className="text-[12px] text-[#8A87AA]">
+            ${formatNumber(price, 2, true)}
+          </div>
         </div>
       </div>
       <div>
         <div className="text-[14px] text-black">
-          {formatNumber(balance, 2, true)}
+          {formatNumber(balance || 0, 2, true)}
         </div>
         <div className="text-[12px] text-[#8A87AA]">
-          ${formatNumber(Big(balance).mul(price).toNumber(), 2, true)}
+          $
+          {formatNumber(
+            Big(balance || 0)
+              .mul(price)
+              .toNumber(),
+            2,
+            true
+          )}
         </div>
       </div>
     </div>
