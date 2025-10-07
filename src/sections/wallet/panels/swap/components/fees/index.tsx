f397e63fd78199120047ca02b2a7abc0bee46d47
Luna <luna.spark9234@proton.me>
2025-10-07 19:23:23 +0800

    fix swap animation.

--- src/sections/wallet/panels/swap/components/fees/index.tsx
+++ src/sections/wallet/panels/swap/components/fees/index.tsx
@@ -26,35 +26,37 @@ export default function Routes({
           initial={{ opacity: 0, height: 0 }}
           animate={{ opacity: 1, height: 120 }}
           exit={{ opacity: 0, height: 0 }}
-          className="border border-[#373A53] rounded-[12px] mt-[10px] p-[10px]"
+          className="border border-[#373A53] rounded-[12px] mt-[10px]"
         >
-          <Fee
-            name="Price impact"
-            value={`${priceImpact || "-"}%`}
-            valueClassName={COLOR[priceImpactType || 0]}
-          />
-          <Fee
-            name="Minimum received"
-            valueClassName="text-[#3B3951]"
-            value={Big(outputCurrencyAmount || 0)
-              .mul(1 - slippage / 100)
-              .toFixed(8)}
-          />
-          <Fee
-            name="Route"
-            valueClassName="text-[#3B3951]"
-            value={
-              <div className="flex items-center gap-2">
-                <img
-                  src="/near-intents-logo.png"
-                  alt={name}
-                  width={80}
-                  height={20}
-                />
-                <span>{routerStr}</span>
-              </div>
-            }
-          />
+          <div className="p-[10px]">
+            <Fee
+              name="Price impact"
+              value={`${priceImpact || "-"}%`}
+              valueClassName={COLOR[priceImpactType || 0]}
+            />
+            <Fee
+              name="Minimum received"
+              valueClassName="text-[#3B3951]"
+              value={Big(outputCurrencyAmount || 0)
+                .mul(1 - slippage / 100)
+                .toFixed(8)}
+            />
+            <Fee
+              name="Route"
+              valueClassName="text-[#3B3951]"
+              value={
+                <div className="flex items-center gap-2">
+                  <img
+                    src="/near-intents-logo.png"
+                    alt={name}
+                    width={80}
+                    height={20}
+                  />
+                  <span>{routerStr}</span>
+                </div>
+              }
+            />
+          </div>
         </motion.div>
       )}
     </AnimatePresence>
