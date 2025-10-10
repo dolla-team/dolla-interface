be4162aef7b58f6483cbb4bc82f2fd5a6aa8420e
Luna <luna.spark9234@proton.me>
2025-10-10 18:03:40 +0800

    fix 1166.

--- src/views/btc/detail/end/player-distribution.tsx
+++ src/views/btc/detail/end/player-distribution.tsx
@@ -20,10 +20,10 @@ export default function PlayerDistribution({
   }, [distributed]);
 
   return (
-    <div className="w-full h-full rounded-[10px] bg-[#00000033] mt-[10px]">
+    <div className="w-full rounded-[10px] bg-[#00000033] mt-[10px]">
       <div
         className={clsx(
-          "h-full pt-[12px] pb-[20px] flex flex-col justify-between",
+          "pt-[14px] pb-[20px] flex flex-col gap-[12px]",
           isMobile ? "px-[0px]" : "px-[18px]"
         )}
       >
@@ -49,33 +49,35 @@ const Item = ({ data, number, total, winner, bidsDistribution }: any) => {
   }, [number, total]);
 
   return (
-    <div className="flex items-center">
-      <div className="text-[14px] text-[#FFE9B2] w-[100px] mr-[11px] shrink-0">
-        BID x{data.value}
+    number && (
+      <div className="flex items-center">
+        <div className="text-[14px] text-[#FFE9B2] w-[100px] mr-[11px] shrink-0">
+          BID x{data.value}
+        </div>
+        <div className="h-[12px] w-[292px] rounded-[6px] flex items-center">
+          <div
+            className="h-full rounded-[6px] mr-[16px]"
+            style={{ width: `${width}%`, backgroundColor: data.color }}
+          />
+          {!!bidsDistribution && (
+            <>
+              <div className="text-[14px] text-[#ADBCCF]">{number}</div>
+              <div className="flex items-center gap-[6px] ml-[10px]">
+                <Avatar
+                  size={20}
+                  src={winner?.icon}
+                  email={winner?.show_email}
+                  address={winner?.user}
+                  className="text-[12px]"
+                />
+                <span className="text-[12px] text-white">
+                  x{bidsDistribution}
+                </span>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-      <div className="h-[12px] w-[292px] rounded-[6px] flex items-center">
-        <div
-          className="h-full rounded-[6px] mr-[16px]"
-          style={{ width: `${width}%`, backgroundColor: data.color }}
-        />
-        {!!bidsDistribution && (
-          <>
-            <div className="text-[14px] text-[#ADBCCF]">{number}</div>
-            <div className="flex items-center gap-[6px] ml-[10px]">
-              <Avatar
-                size={20}
-                src={winner?.icon}
-                email={winner?.show_email}
-                address={winner?.user}
-                className="text-[12px]"
-              />
-              <span className="text-[12px] text-white">
-                x{bidsDistribution}
-              </span>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
+    )
   );
 };
