e3599b63b21aab8629cb6c336f27f5d92044b21d
Luna <luna.spark9234@proton.me>
2025-10-16 12:54:29 +0800

    open a new window for docs.

--- src/views/btc-list/banner.tsx
+++ src/views/btc-list/banner.tsx
@@ -1,9 +1,7 @@
 import Button from "@/components/button";
 import DollaEye from "@/components/dolla-eye";
-import { useNavigate } from "react-router-dom";
 
 export default function Banner() {
-  const navigate = useNavigate();
   return (
     <div className="w-full h-[300px] relative rounded-[20px] bg-[#000] bg-[url('/home/banner-btc-bg.png')] bg-cover bg-center pl-[40px] pt-[30px]">
       <div className="text-[50px] text-white font-[700] mt-[-10px]">
@@ -14,7 +12,7 @@ export default function Banner() {
       </div>
       <Button
         onClick={() => {
-          navigate("/docs");
+          window.open(window.location.origin + "/docs", "_blank");
         }}
         className="w-[140px] h-[42px] mt-[80px] !bg-[#FFC42F]"
       >
