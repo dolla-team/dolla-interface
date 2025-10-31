b6cdf80ab562e194f4ebfec6fb004914d38dc183
Luna <luna.spark9234@proton.me>
2025-10-31 17:45:16 +0800

    fix bid hints.

--- src/hooks/near/use-bid.ts
+++ src/hooks/near/use-bid.ts
@@ -47,7 +47,7 @@ export default function useBid(
   };
 
   const onBid = async (times: number) => {
-    // onTxSuccess();
+    // onTxSuccess("784");
 
     // setTimeout(() => {
     //   onSuccess({
--- src/views/btc/components/bid-hints/index.tsx
+++ src/views/btc/components/bid-hints/index.tsx
@@ -4,14 +4,14 @@ import { formatAddress } from "@/utils/format/address";
 import { useBtcContext } from "../../context";
 
 export default function BidHints({ taskId }: { taskId: string }) {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
   const [isVisible, setIsVisible] = useState(false);
   const [txHash, setTxHash] = useState("");
   const { flipStatus } = useBtcContext();
 
   useEffect(() => {
     if (!taskId) {
-      setStep(1);
+      setStep(0);
       return;
     }
     const fetchBidHints = async () => {
@@ -30,16 +30,21 @@ export default function BidHints({ taskId }: { taskId: string }) {
         }, 1000);
       } else {
         clearTimeout(window.bidHintsTimer);
-        setStep(1);
       }
     };
     fetchBidHints();
     return () => clearInterval(window.bidHintsTimer);
   }, [taskId]);
 
+  useEffect(() => {
+    if (flipStatus === 0) {
+      setStep(0);
+    }
+  }, [flipStatus]);
+
   // Trigger slide-up animation when step changes
   useEffect(() => {
-    if (step > 1 || flipStatus > 0) {
+    if (step > 0) {
       // Reset visibility to trigger animation
       setIsVisible(false);
       // Use requestAnimationFrame to ensure DOM update before animation
@@ -66,7 +71,7 @@ export default function BidHints({ taskId }: { taskId: string }) {
 
   const secondaryText =
     step === 1
-      ? "Bid #84732"
+      ? `Bid #${taskId}`
       : step === 2
       ? "TEE is generating your signature"
       : step === 3
@@ -76,10 +81,10 @@ export default function BidHints({ taskId }: { taskId: string }) {
       : "";
 
   return (
-    (step > 1 || flipStatus > 0) && (
+    step > 0 && (
       <div
         key={step}
-        className={`absolute top-0 left-1/2 -translate-x-1/2 flex flex-col justify-center items-center w-[516px] h-[72px] rounded-[16px] border border-[#605D55] bg-[#FFFFFF1A] backdrop-blur-[25px] transition-all duration-[400ms] ease-out ${
+        className={`absolute z-[50] top-0 left-1/2 -translate-x-1/2 flex flex-col justify-center items-center w-[516px] h-[72px] rounded-[16px] border border-[#605D55] bg-[#FFFFFF1A] backdrop-blur-[25px] transition-all duration-[400ms] ease-out ${
           isVisible
             ? "translate-y-0 opacity-100"
             : "translate-y-[20px] opacity-0"
