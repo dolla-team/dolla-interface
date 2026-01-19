b40312c454e748e488906f86d9b03577f589d486
Luna <luna.spark9234@proton.me>
2026-01-19 15:21:16 +0800

    fix hints.

--- src/sections/x-kol/index.tsx
+++ src/sections/x-kol/index.tsx
@@ -15,7 +15,7 @@ export default function XKolModal() {
           Congrats!
         </div>
         <div className="text-[16px] font-[400] text-black text-center mt-[20px]">
-          You have $5 voucher to be redeemed
+          You have ${reward} voucher to be redeemed
         </div>
         <div className="w-[340px] h-[95px] mt-[20px] mx-auto pl-[60px] flex items-center justify-center bg-[url('/kol/x-kol-ticket.png')] bg-no-repeat bg-center bg-contain">
           <span className="text-[32px] text-black font-[Courier]">
