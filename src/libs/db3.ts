8a08e0c2fb3e2a3ee1db1c07e902be4fead383e2
Luna <luna.spark9234@proton.me>
2026-01-22 19:17:04 +0800

    replace report path.

--- src/libs/db3.ts
+++ src/libs/db3.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function report(params: any) {
   try {
-    await axios.post("http://api.stableflow.ai/v1/trade/add", params);
+    await axios.post("https://api.stableflow.ai/v1/trade/add", params);
   } catch (error) {
     console.log("report failed: %o", error);
   }
