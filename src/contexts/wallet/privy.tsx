086ab46a534d3894b4f1ad9099ba9ba820640511
Luna <luna.spark9234@proton.me>
2026-01-28 19:29:19 +0800

    fix social login.

--- src/contexts/wallet/privy.tsx
+++ src/contexts/wallet/privy.tsx
@@ -1,5 +1,7 @@
 import { PrivyProvider } from "@privy-io/react-auth";
 import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+import useLoginStore from '@/stores/use-login'
+import { useMemo } from 'react'
 
 export default function WalletProvider({
   children
@@ -7,42 +9,50 @@ export default function WalletProvider({
   children: React.ReactNode;
 }) {
   const solanaConnectors = toSolanaWalletConnectors();
+  const loginStore = useLoginStore()
+
+  const loginMethods = useMemo(() => {
+    if (loginStore.isX) {
+      return ['twitter']
+    }
+    return ['email', 'google', 'twitter', 'wallet']
+  }, [loginStore.isX]) 
   return (
     <PrivyProvider
       appId={import.meta.env.VITE_PRIVY_APP_ID as string}
       clientId={import.meta.env.VITE_PRIVY_CLIENT_ID as string}
       config={{
         appearance: {
-          accentColor: "#FFC42F",
-          theme: "#fff",
+          accentColor: '#FFC42F',
+          theme: '#fff',
           showWalletLoginFirst: false,
-          logo: "/logo.svg",
-          walletChainType: "ethereum-and-solana",
-          loginMessage: "Enter your email to receive a secure code",
-          walletList: ["detected_ethereum_wallets", "detected_solana_wallets"]
+          logo: '/logo.svg',
+          walletChainType: 'ethereum-and-solana',
+          loginMessage: 'Enter your email to receive a secure code',
+          walletList: ['detected_ethereum_wallets', 'detected_solana_wallets'],
         },
-        loginMethods: ["email", "google", "twitter", "wallet"],
+        loginMethods: loginMethods as any,
         fundingMethodConfig: {
           moonpay: {
-            useSandbox: true
-          }
+            useSandbox: true,
+          },
         },
         embeddedWallets: {
-          showWalletUIs: true
+          showWalletUIs: true,
         },
         mfa: {
-          noPromptOnMfaRequired: false
+          noPromptOnMfaRequired: false,
         },
         externalWallets: {
           solana: {
-            connectors: solanaConnectors
-          }
-        }
+            connectors: solanaConnectors,
+          },
+        },
       }}
     >
       {children}
     </PrivyProvider>
-  );
+  )
 }
 
 export const useGelatoSmartWalletPrivyContext = () => {
