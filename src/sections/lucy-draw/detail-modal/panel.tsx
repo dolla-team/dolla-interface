657339d7e00fc1d0b2287a35b8478c99f66193a8
Luna <luna.spark9234@proton.me>
2026-01-29 17:43:42 +0800

    lucky draw hints.

--- src/sections/lucy-draw/detail-modal/panel.tsx
+++ src/sections/lucy-draw/detail-modal/panel.tsx
@@ -188,11 +188,11 @@ export const LuckyInfo = () => {
         <div className="w-[298px] text-[#5E6B7D] text-[12px] font-[300] p-[16px] bg-white rounded-[10px] border border-[#E4E4E4]">
           <div className="font-[500] text-black mb-[4px]">Lucky Draw</div>{' '}
           <div>
-            Earn tickets from your bids for a chance to win part of the $X daily pool. 100 winners
-            every 24h — tickets reset each round. <br />
+            Earn tickets from your bids for a chance to win part of the 100 USDT daily pool every
+            24h — tickets reset each round. <br />
             🥇1st winner: 50 USDT <br />
-            🥈2st winner: 15 USDT <br />
-            🥉3st winner: 10 USDT <br />
+            🥈2nd winner: 15 USDT <br />
+            🥉3rd winner: 10 USDT <br />
             🍀Lucky players: 5 USDT
           </div>
         </div>
